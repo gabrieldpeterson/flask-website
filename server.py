bed4418f55e62928acd12629175efe7a68fb95ee
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-07-05 18:22:23 -0700

    Change template, and update server.py to route to the appropriate pages

--- server.py
+++ server.py
@@ -4,5 +4,20 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def my_home():
     return render_template('index.html')
+
+
+@app.route("/index.html")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/elements.html")
+def elements():
+    return render_template('elements.html')
+
+
+@app.route("/generic.html")
+def generic():
+    return render_template('generic.html')
