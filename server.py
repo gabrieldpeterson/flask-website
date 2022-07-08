9f745b2117b5bfb0038770c5af850c4cc880413f
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-07-07 17:02:50 -0700

    Create a route for when the send message button is clicked on the contact page

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -11,3 +11,8 @@ def my_home():
 @app.route("/<string:page_name>")
 def index(page_name):
     return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    return #render_template('login.html', error=error)
