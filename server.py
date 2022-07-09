24ee352dcd9f2d59e187be381e4b12701e7f80b5
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-07-08 17:48:03 -0700

    Have the form write the data to an external text file

--- server.py
+++ server.py
@@ -13,11 +13,17 @@ def index(page_name):
     return render_template(page_name)
 
 
+def write_to_file(ddata):
+    with open('database.txt', 'a') as f:
+        save_text = f'{ddata["name"]},{ddata["email"]},{ddata["message"]}\n'
+        f.write(save_text)
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        write_to_file(data)
         return redirect('/thankyou.html')
     else:
         return 'something went wrong'
