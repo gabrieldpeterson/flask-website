2f470286581a7693c19b02f87304a852e85920c7
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-07-08 18:06:19 -0700

    Have the form write the data to an external csv file

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import csv
 
 app = Flask(__name__)
 
@@ -19,11 +20,20 @@ def write_to_file(ddata):
         f.write(save_text)
 
 
+def write_to_csv(ddata):
+    with open('database.csv', newline='', mode='a') as f:
+        name = ddata["name"]
+        email = ddata["email"]
+        message = ddata["message"]
+        csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([name, email, message])
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        write_to_file(data)
+        write_to_csv(data)
         return redirect('/thankyou.html')
     else:
         return 'something went wrong'
